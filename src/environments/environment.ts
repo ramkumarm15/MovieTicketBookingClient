cbc985ce9fe1752bd5c639324adbcd542eff64d1
Ramkumar M <ramkumarmani2000@gmail.com>
2023-07-04 10:37:12 +0530

    api production url changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://movieticketbooking-prod.azurewebsites.net/api/v1'
+  apiBaseUrl: 'https://web-client-gateway.azure-api.net/api/v1'
 };
