9bd8526b6570923b9eac90282a8ff8911d834a6c
Ramkumar <64207937+ramkumarm15@users.noreply.github.com>
2023-10-05 17:53:13 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://web-client-gateway.azure-api.net/api/v1'
+  apiBaseUrl: 'https://movieticket-booking-server.azurewebsites.net/api/v1'
 };
